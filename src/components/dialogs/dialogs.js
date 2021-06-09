c47a3a44627525e281f63f51b3df3f3a9bb5e795
unknown <juliesg@tut.by>
2021-06-09 23:24:06 +0300

    dialogs less 21

--- src/components/dialogs/dialogs.js
+++ src/components/dialogs/dialogs.js
@@ -2,7 +2,21 @@ import React from "react";
 import style from "./dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>dialogs</div>;
+  return (
+    <div className={style.dialogs}>
+      <div className={style.dialogItems}>
+        <div className={style.dialog + " " + style.active}>Marina</div>
+        <div className={style.dialog}>Irina</div>
+        <div className={style.dialog}>Sergey</div>
+        <div className={style.dialog}>Michail</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>Hello</div>
+        <div className={style.message}>Good morning</div>
+        <div className={style.message}>Yo!!</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
