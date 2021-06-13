020105a14ee0165e517ce1a64763479f685da934
unknown <juliesg@tut.by>
2021-06-14 00:11:27 +0300

    map 25

--- src/components/dialogs/dialogs.js
+++ src/components/dialogs/dialogs.js
@@ -15,40 +15,33 @@ const Message = (props) => {
   return <div className={style.message}>{props.message}</div>;
 };
 
-let dialogsData = [
-  { id: 1, name: "Marina" },
-  { id: 2, name: "Olga" },
-  { id: 3, name: "Michail" },
-  { id: 4, name: "Lilia" },
-  { id: 5, name: "Boss" },
-];
-
-let messageData = [
-  { id: 1, message: "Hello" },
-  { id: 2, message: "Good morning" },
-  { id: 3, message: "Yo!!" },
-  { id: 4, message: "Yo!!" },
-  { id: 5, message: "Yo!!" },
-];
-
 const Dialogs = (props) => {
+  let dialogs = [
+    { id: 1, name: "Marina" },
+    { id: 2, name: "Olga" },
+    { id: 3, name: "Michail" },
+    { id: 4, name: "Lilia" },
+    { id: 5, name: "Boss" },
+  ];
+
+  let messages = [
+    { id: 1, message: "Hello" },
+    { id: 2, message: "Good morning" },
+    { id: 3, message: "Yo!!" },
+    { id: 4, message: "Yo!!" },
+    { id: 5, message: "Yo!!" },
+  ];
+
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messageElements = messages.map((m) => <Message message={m.message} />);
+
   return (
     <div className={style.dialogs}>
-      <div className={style.dialogItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-      </div>
-
-      <div className={style.messages}>
-        <Message message={messageData[0].message} />
-        <Message message={messageData[1].message} />
-        <Message message={messageData[2].message} />
-        <Message message={messageData[3].message} />
-        <Message message={messageData[4].message} />
-      </div>
+      <div className={style.dialogItems}>{dialogsElements}</div>
+      <div className={style.messages}>{messageElements}</div>
     </div>
   );
 };
--- src/components/profile/MyPosts/Post/MyPosts.js
+++ src/components/profile/MyPosts/Post/MyPosts.js
@@ -2,12 +2,18 @@ import React from "react";
 import style from "./MyPosts.module.css";
 import Post from "@comp/profile/MyPosts/Post/Post";
 
-let postData = [
-  { id: 1, message: "Hi! How are you?", countLike: 12 },
-  { id: 2, message: "It`s my first post", countLike: 34 },
-];
-
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: "Hi! How are you?", countLike: 12 },
+    { id: 2, message: "It`s my first post", countLike: 34 },
+    { id: 3, message: "Today is a good day", countLike: 58 },
+    { id: 4, message: "Sun", countLike: 7 },
+  ];
+
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} countLike={p.countLike} />
+  ));
+
   return (
     <div className={style.postBlocks}>
       <h3>My posts</h3>
@@ -19,8 +25,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <Post message={postData[0].message} countLike={postData[0].countLike} />
-      <Post message={postData[1].message} countLike={postData[1].countLike} />
+      {postsElements}
     </div>
   );
 };
